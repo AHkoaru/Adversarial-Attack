a8470757b5a5f2836e21c5be3beb9b210967eb70
GODAEYUN <eodbs117848@gmail.com>
2025-03-19 09:34:03 +0000

    utils update

--- utils.py
+++ utils.py
@@ -48,3 +48,31 @@ def save_results(results, filename):
     with open(file_path, "w") as f:
         json.dump(existing_results, f, ensure_ascii=False, indent=4)
     print(f"결과가 '{file_path}' 파일에 추가 저장되었습니다.")
+
+import numpy as np
+import torch
+from torch import nn
+
+def compute_metrics(eval_pred, metric, num_labels=19):
+    with torch.no_grad():
+        logits, labels = eval_pred
+        logits_tensor = torch.from_numpy(logits)
+        logits_tensor = nn.functional.interpolate(
+            logits_tensor,
+            size=labels.shape[-2:],
+            mode="bilinear",
+            align_corners=False,
+        ).argmax(dim=1)
+
+        pred_labels = logits_tensor.detach().cpu().numpy()
+        metrics = metric.compute(
+            predictions=pred_labels,
+            references=labels,
+            num_labels=num_labels,
+            ignore_index=255,
+            reduce_labels=False,
+        )
+        for key, value in metrics.items():
+            if isinstance(value, np.ndarray):
+                metrics[key] = value.tolist()
+        return metrics
