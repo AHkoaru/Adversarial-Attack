df2489d4d764bffa22c83563872c68ea59b148e6
GODAEYUN <eodbs117848@gmail.com>
2025-03-24 15:09:04 +0000

    mmseg setting

---
+++ test.sh
@@ -0,0 +1,3 @@
+python tools/test.py \
+configs/segformer/segformer_mit-b0_8xb1-160k_cityscapes-1024x1024.py \
+/mmsegmentation/checkpoint/segformer_mit-b0_8x1_1024x1024_160k_cityscapes_20211208_101857-e7f88502.pth
