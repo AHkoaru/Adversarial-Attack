db4e0094405ddc14e472fc90a2b9be50571f57cf
GoDaeYun <eodbs117848@gmail.com>
2025-05-07 23:40:44 +0900

    config

--- configs_attack/cityscapes/config_seg.py
+++ configs_attack/cityscapes/config_seg.py
@@ -7,7 +7,6 @@ config = {
     "base_dir": "./data/PixelAttack/results/cityscapes/segformer",  # Base directory for saving results
     "model": "segformer",
     "RGB": 3,                                       # Input dimension
-    "gt_dir": "datasets/cityscapes/gtFine",           # Directory for Ground-Truth images
     "attack_pixel": 0.01,                              # Attack dimension for the Remember process (recalculated later)
     "num_class": 19,
 }
