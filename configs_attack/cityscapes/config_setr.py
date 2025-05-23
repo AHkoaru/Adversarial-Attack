d8f8aedd1c9adc3a2ad895e04852fb217843cf55
GoDaeYun <eodbs117848@gmail.com>
2025-05-23 14:57:54 +0900

    add setr code

--- configs_attack/cityscapes/config_setr.py
+++ configs_attack/cityscapes/config_setr.py
@@ -4,8 +4,8 @@ config = {
     "task": "segmentation",
     "dataset": "cityscapes",
     "data_dir": "datasets/cityscapes",         # Directory path where the dataset is located
-    "base_dir": "./data/PixelAttack/results/cityscapes/ster",  # Base directory for saving results
-    "model": "ster",
+    "base_dir": "./data/PixelAttack/results/cityscapes/setr",  # Base directory for saving results
+    "model": "setr",
     "RGB": 3,                                       # Input dimension
     "attack_pixel": 0.01,                              # Attack dimension for the Remember process (recalculated later)
     "num_class": 19,
