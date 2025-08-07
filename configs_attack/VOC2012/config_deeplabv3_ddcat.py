efe2221e91d3e775f275983d4c242d4526d76a35
GoDaeYun <eodbs117848@gmail.com>
2025-08-08 00:30:49 +0900

    config file

---
+++ configs_attack/VOC2012/config_deeplabv3_ddcat.py
@@ -0,0 +1,10 @@
+config = {
+    "attack_method": "Pixel",
+    "task": "segmentation",
+    "dataset": "VOC2012",
+    "data_dir": "datasets/VOC2012",         # Directory path where the dataset is located
+    "model": "deeplabv3_ddcat",
+    "RGB": 3,                                       # Input dimension
+    "attack_pixel": 0.01,                              # Attack dimension for the Remember process (recalculated later)
+    "num_class": 21,
+}
