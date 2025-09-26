124bc0d0439397f1f25a1bc2bcfcd900fad7ae9a
GoDaeYun <eodbs117848@gmail.com>
2025-09-26 16:44:01 +0900

    remove useless code

--- test.sh
+++ test.sh
@@ -12,26 +12,55 @@
 #configs_attack/cityscapes/config_setr.py
 # export CUDA_VISIBLE_DEVICES=0
 
+# python px_eval.py \
+#     --config configs_attack/ade20k/config_pspnet.py\
+#     --attack_pixel 0.05
 
-python rs_eval.py \
-    --config configs_attack/voc2012/config_pspnet.py\
-    --iter 5\
-    --num_images 3
+# python px_eval.py \
+#     --config configs_attack/ade20k/config_deeplabv3.py\
+#     --attack_pixel 0.05
+
+# python rs_eval.py \
+#     --config configs_attack/cityscapes/config_pspnet.py
 
 
-python rs_eval.py \
-    --config configs_attack/voc2012/config_seg.py\
-    --norm patches\
-    --eps 0.05\
+# python rs_eval.py \
+#     --config configs_attack/cityscapes/config_deeplabv3.py
 
 
-python rs_eval.py \
-    --config configs_attack/ade20k/config_pspnet.py\
-    --norm patches\
-    --eps 0.04
+# python px_eval.py \
+#     --config configs_attack/VOC2012/config_deeplabv3.py\
+#     --attack_pixel 0.05
 
-python rs_eval.py \
-    --config configs_attack/cityscapes/config_setr.py
+
+# python rs_eval.py \
+#     --config configs_attack/ade20k/config_pspnet.py\
+#     --norm patches\
+#     --eps 0.04
+
+# python rs_eval.py \
+#     --config configs_attack/ade20k/config_pspnet.py
+
+# python rs_eval.py \
+#     --config configs_attack/cityscapes/config_deeplabv3.py
+
+python px_eval.py \
+    --config configs_attack/cityscapes/config_deeplabv3.py\
+    --attack_pixel 0.05
+
+python px_eval.py \
+    --config configs_attack/cityscapes/config_pspnet.py\
+    --attack_pixel 0.05
+
+python px_eval.py \
+    --config configs_attack/cityscapes/config_seg.py\
+    --attack_pixel 0.05
+
+# python px_eval.py \
+#     --config configs_attack/cityscapes/config_seg.py\
+#     --attack_pixel 0.05
+
+
 
 # python rs_eval.py \
 #     --config configs_attack/cityscapes/config_deepdlabv3.py
